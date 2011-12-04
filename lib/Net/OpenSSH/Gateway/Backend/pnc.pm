fecd83b0cc4d8ca7066f9da12faa3cf77d80b031
Salvador Fandino <sfandino@yahoo.com>
2011-12-04 23:31:36 +0100

    add backend for pnc

---
+++ lib/Net/OpenSSH/Gateway/Backend/pnc.pm
@@ -0,0 +1,42 @@
+package Net::OpenSSH::Gateway::Backend::pnc;
+
+use strict;
+use warnings;
+
+require Net::OpenSSH::Gateway::Backend;
+our @ISA = qw(Net::OpenSSH::Gateway::Backend);
+
+sub _command { 'pnc' }
+
+sub _command_version_args { '-V' }
+
+sub _check_command_version_output {
+    my ($self, $text) = @_;
+    if (my ($ver) = $text =~ /^pnc\s+(\S+)/m) {
+        $self->{pnc_version} = $ver;
+        $self->{pnc_version_number} = $self->_version_to_number($ver);
+        return 1;
+    }
+    return;
+}
+
+sub check_args {
+    my $self = shift;
+    my $proxies = $self->{proxies};
+    if (@$proxies) {
+        $self->_push_error("pnc does not support proxies");
+        return;
+    }
+    1;
+}
+
+sub _command_args {
+    my ($self, %opts) = @_;
+
+    my $host = $self->_slave_quote_opt(host => %opts);
+    my $port = $self->_slave_quote_opt(port => %opts);
+
+    return ($host, $port);
+}
+
+1;
