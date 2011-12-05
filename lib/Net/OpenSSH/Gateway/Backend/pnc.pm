b0a2ed17aaafc02a7ebf2f2b64d241025733ef2e
Salvador <sfandino@yahoo.com>
2011-12-05 14:15:07 +0100

    forbid no via_ssh usage and remove broken and useless command version tests

--- lib/Net/OpenSSH/Gateway/Backend/pnc.pm
+++ lib/Net/OpenSSH/Gateway/Backend/pnc.pm
@@ -8,20 +8,12 @@ our @ISA = qw(Net::OpenSSH::Gateway::Backend);
 
 sub _command { 'perl' }
 
-sub _command_version_args { '-V' }
-
-sub _check_command_version_output {
-    my ($self, $text) = @_;
-    if (my ($ver) = $text =~ /^pnc\s+(\S+)/m) {
-        $self->{pnc_version} = $ver;
-        $self->{pnc_version_number} = $self->_version_to_number($ver);
-        return 1;
-    }
-    return;
-}
-
 sub check_args {
     my $self = shift;
+    unless ($self->{via_ssh}) {
+        $self->_push_error("using pnc does not make sense without via_ssh");
+        return;
+    }
     my $proxies = $self->{proxies};
     if (@$proxies) {
         $self->_push_error("pnc does not support proxies");
@@ -30,21 +22,33 @@ sub check_args {
     1;
 }
 
+sub _remote_pnc {
+    my $self = shift;
+    my $cmd = $self->{private}{remote_pnc};
+    defined $cmd ? $cmd : ".pnc.pl";
+}
+
 sub _command_args {
     my ($self, %opts) = @_;
 
     my $host = $self->_slave_quote_opt(host => %opts);
     my $port = $self->_slave_quote_opt(port => %opts);
 
-    return ($host, $port);
+    my $remote_pnc = $self->_slave_quote($self->_remote_pnc);
+    return ($remote_pnc, $host, $port);
 }
 
-sub _before_ssh_connect {
-    
-}
-
-sub _after_ssh_connect {
-
+sub before_ssh_connect {
+    my $self = shift;
+    my $sftp;
+    if (my $ssh = $self->{via_ssh}) {
+        require App::pnc;
+        unless ($ssh->scp_put($INC{"App/pnc.pm"}, $self->_remote_pnc)) {
+            $self->_push_error("scp failed", $ssh->error);
+            return;
+        }
+    }
+    $self->SUPER::before_ssh_connect;
 }
 
 1;
